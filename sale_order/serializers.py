4ebbc1ec08c3ef975c7b116dcbf9fee233026786
Mohammed Darwish <muhadar.univ@gmail.com>
2025-09-01 10:13:41 +0300

    Add serializer for the model

---
+++ sale_order/serializers.py
@@ -0,0 +1,26 @@
+from .models import Orders_details
+from rest_framework import serializers
+class OrdersDetailsSerializer(serializers.ModelSerializer):
+    """
+    Serializer for user orders details.
+    """
+    sale_order_id = serializers.IntegerField(
+        required=True,
+    )
+    order_quantity = serializers.IntegerField(required = True,)
+    stockexchange_quantity = serializers.IntegerField()
+    quantity_post_market = serializers.IntegerField()
+    quantity_at_market = serializers.IntegerField()
+
+
+    class Meta:
+        model = Orders_details
+        fields = ('sale_order_id', 'order_quantity', 'stockexchange_quantity' , 'quantity_post_market', 'quantity_at_market')
+    def validate(self , data ) :
+      order_qty = data.get('order_quantity')
+      if order_qty < 0 :
+        raise serializers.ValidationError("Not A Valid Order")
+      elif order_qty < (data.get('stockexchange_quantity') + data.get('quantity_at_market')) :
+         raise serializers.ValidationError("Requested quantity is more than the order quantity")
+      else :
+        return data
