c640840fbee2e328373dd3e0994f50bc4a75ab9c
Mohammed Darwish <muhadar.univ@gmail.com>
2025-09-09 15:21:27 +0300

    update celery config

--- demoapp/celery.py
+++ demoapp/celery.py
@@ -19,3 +19,5 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.conf.timezone = "UTC"
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
+
+app.conf.broker_connection_retry_on_startup = True
